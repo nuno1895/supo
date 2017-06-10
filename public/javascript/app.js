724f575f7763b377df0dc495da456822dede1c43
Steven Gissubel <steven.gissubel@gmail.com>
2017-06-10 19:23:11 -0400

    nav transitions in all buget forms -- also fixed other layout issues

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     var scroll_start = 0;
     var jumbotron = $('.jumbotron');
     var offset = jumbotron.offset();
+    var budgetHead = $(".budgetHead");
+    var budgetOffset = budgetHead.offset();
+
     if (jumbotron.length){
         $(document).scroll(function(){
             scroll_start = $(this).scrollTop();
@@ -12,6 +15,19 @@ $(document).ready(function(){
             }
         });
     }
+
+    if (budgetHead.length){
+        $(document).scroll(function(){
+            scroll_start = $(this).scrollTop();
+            if(scroll_start > budgetOffset.top) {
+                $(".container-fluid").css('background-color', '#292b2c').css('padding', '0');
+            } else {
+                $(".container-fluid").css('background-color', 'rgba(41, 43, 44, .1)').css('padding', '1em');                
+            }
+        });
+    }
+
+
 });
 
 window.onscroll = function() {
