4c5d00d16f23b3ee929aa706208a424a8894f716
Steven Gissubel <steven.gissubel@gmail.com>
2017-06-12 18:04:21 -0400

    got the label to appear with the correct input

--- public/javascript/form7.js
+++ public/javascript/form7.js
@@ -1,3 +1,10 @@
+var medBill = {
+    'otherMedical1': 'Other Medical 1',
+    'otherMedical2': 'Other Medical 2',
+    'otherMedical3': 'Other Medical 3'
+};
+
+
 $("#budgetBtn7").on("click", function() {
     var budgetObject = {
     	medications: $("#medications").val(),
@@ -14,29 +21,34 @@ $("#budgetBtn7").on("click", function() {
 });
 
 $("#addMedBill").on("click", function(){
-    var medBill = {
-        'otherMedical1': 'Other Medical 1',
-        'otherMedical2': 'Other Medical 2',
-        'otherMedical3': 'Other Medical 3'
-    };
+
     var s = $("<select class='form-control' id='medAddOn' />")
         $("<option/>", {text:'Choose an Medical Add On Option'}).appendTo(s);
 
         for (var val in medBill){
-            $("<option/>", {value: val, id: val, class: 'medBillAddOpt', text: medBill[val]}).appendTo(s);
+            $("<option/>", {value: val, id: val, class: 'medBillAddOpt', text: medBill[val]}).attr('data', medBill[val],).appendTo(s);
         }
     s.appendTo('.addMedBillDiv');
 
 });
 
 $(document).on('change', "#medAddOn", function() {
+    debugger;
+    console.log(this.data);
     if(this.value == 'otherMedical1' || this.value == 'otherMedical2' || this.value == 'otherMedical3'){
-        console.log('YES!');
+        $('.addMedBillDiv').append("<label for='" + this.value +"'>" + $(this).find(':selected').attr('data') + "</label>");
         $('.addMedBillDiv').append("<input id='" + this.value + "'" + " " + "class='form-control medInpAdd'/>");
     } else{
         console.log('whoopsiedoodle')
     }
-})
+});
+// $(document).on('change', "#medAddOn", function() {
+//     if(this.value == 'otherMedical1' || this.value == 'otherMedical2' || this.value == 'otherMedical3'){
+//         $('.addMedBillDiv').append("<input id='" + this.value + "'" + " " + "class='form-control medInpAdd'/>");
+//     } {
+//         console.log('whoopsiedoodle')
+//     }
+// });
 
 
 $("#budgetBtn7Back").on("click", function() {
