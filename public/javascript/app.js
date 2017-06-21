3aa99f166bd0323abedfb59ba4f5e88a559e8d44
Steven Gissubel <steven.gissubel@gmail.com>
2017-06-21 16:32:42 -0400

    fixed up budget pages

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -12,6 +12,8 @@ $(document).ready(function(){
     var topSignInOffset = topSignIn.offset();
     var topSignup = $(".signupDiv");
     var topSignupOffset = topSignup.offset();
+    var dashHead = $(".dashHead");
+    var dashHeadOffSet = dashHead.offset();
 
     if (jumbotron.length){
         $(document).scroll(function(){
@@ -30,7 +32,7 @@ $(document).ready(function(){
             if(scroll_start > budgetOffset.top) {
                 $(".container-fluid").css('background-color', '#292b2c').css('padding', '0');
             } else {
-                $(".container-fluid").css('background-color', 'rgba(41, 43, 44, .1)').css('padding', '1em');                
+                $(".container-fluid").css('background-color', 'rgba(8, 99, 145, 0.3)').css('padding', '1em');                
             }
         });
     }
@@ -52,7 +54,7 @@ $(document).ready(function(){
             if(scroll_start > debtGradeDivOffSet.top) {
                 $(".container-fluid").css('background-color', '#292b2c').css('padding', '0');
             } else {
-                $(".container-fluid").css('background-color', 'rgba(41, 43, 44, .1)').css('padding', '1em');                
+                $(".container-fluid").css('background-color', 'rgba(8, 99, 145, 0.3)').css('padding', '1em');                
             }
         });
     }
@@ -78,22 +80,37 @@ $(document).ready(function(){
         }
     });
     }
-// var monthDropDown = {
-//     'january': 'January',
-//     'february': 'February',
-//     'march': 'March'
-// };
+    
+    if (dashHead.length){
+    $(document).scroll(function(){
+        scroll_start = $(this).scrollTop();
+        if(scroll_start > dashHeadOffSet.top) {
+            $(".container-fluid").css('background-color', '#292b2c').css('padding', '0');
+        } else {
+            $(".container-fluid").css('background-color', 'rgba(41, 43, 44, .1)').css('padding', '1em');                
+        }
+    });
+    }
 
-// var s = $("<select id='month' class='form-control' />")
 
-// for (var val in monthDropDown){
-//     $("<option/>", {value: val, text: monthDropDown[val]}).appendTo(s);
-// }
-
-// s.appendTo('.form-groupMonth');
 
 
 });
+
+var myDate = new Date();
+var hrs = myDate.getHours();
+
+var greet;
+
+    if (hrs < 12)
+        greet = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 17)
+        greet = 'Good Afternoon';
+    else if (hrs >= 17 && hrs <= 24)
+        greet = 'Good Evening';
+
+    $('.greeting').prepend('<b>' + greet + '</b>,' + " ");
+    console.log(greet);
 
 
 window.onscroll = function() {
